6dc291e7ce5e942e30f1fe8d1d41a0187c9cd148
minettodev <minetto_dev@protonmail.com>
2022-10-11 17:29:46 -0300

    Muita coisa

--- Player.gd
+++ Player.gd
@@ -1,43 +1,59 @@
 extends KinematicBody2D
 class_name Player
 
-export(int) var JUMP_FORCE = -160
-export(int) var JUMP_RELEASE_FORCE = -40
-export(int) var MAX_SPEED = 75
-export(int) var ACCELERATION = 10
-export(int) var FRICTION = 10
-export(int) var GRAVITY = 5
-export(int) var ADDITIONAL_FALL_GRAVITY = 2
+enum { MOVE, CLIMB }
+
+export(Resource) var moveData = preload("res://DefaultPlayerMovementData.tres") as PlayerMovementData
 
 var velocity = Vector2.ZERO
+var state = MOVE
+var double_jump = 1
 
 onready var animatedSprite = $AnimatedSprite
+onready var ladderCheck = $LadderCheck
 
 func _physics_process(delta):
-	apply_gravity()
 	var input = Vector2.ZERO
-	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input.x = Input.get_axis("ui_left", "ui_right")
+	input.y = Input.get_axis("ui_up", "ui_down")
 	
+	match state:
+		MOVE: move_state(input)
+		CLIMB: climb_state(input)
+
+func apply_gravity():
+	velocity.y += moveData.GRAVITY
+	velocity.y = min(velocity.y, 300)
+
+func move_state(input):
+	if is_on_ladder() and Input.is_action_pressed("ui_up"):
+		state = CLIMB
+	
+	apply_gravity()
 	if input.x == 0:
 		apply_friction()
 		animatedSprite.animation = "Idle"
 	else:
 		apply_acceleration(input.x)
 		animatedSprite.animation = "Run"
-		if input.x > 0:
-			animatedSprite.flip_h = true
-		elif input.x < 0:
-			animatedSprite.flip_h = false
+		
+		animatedSprite.flip_h = input.x > 0
+	
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
-			velocity.y = JUMP_FORCE
+		double_jump = moveData.DOUBLE_JUMP_COUNT
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = moveData.JUMP_FORCE
 	else:
 		animatedSprite.animation = "Jump"
-		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
-			velocity.y = JUMP_RELEASE_FORCE
+		if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
+			velocity.y = moveData.JUMP_RELEASE_FORCE
+		
+		if Input.is_action_just_pressed("ui_up") and double_jump > 0:
+			velocity.y += moveData.JUMP_FORCE
+			double_jump -= 1
 		
 		if velocity.y > 0:
-			velocity.y += ADDITIONAL_FALL_GRAVITY
+			velocity.y += moveData.ADDITIONAL_FALL_GRAVITY
 	
 	var was_in_air = not is_on_floor() 
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -46,14 +62,26 @@ func _physics_process(delta):
 		animatedSprite.animation = "Run"
 		animatedSprite.frame = 1
 
-func apply_gravity():
-	velocity.y += GRAVITY
-	velocity.y = min(velocity.y, 300)
+func climb_state(input):
+	if not is_on_ladder(): state = MOVE
+	if input.length() != 0:
+		animatedSprite.animation = "Run"
+	else:
+		animatedSprite.animation = "Idle"
+	velocity = input * moveData.CLIMB_SPEED
+	velocity = move_and_slide(velocity, Vector2.UP)
+	pass
+
+func is_on_ladder():
+	if not ladderCheck.is_colliding(): return false
+	var collider = ladderCheck.get_collider()
+	if not collider is Ladder:return false
+	return true
 
 func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, FRICTION)
+	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
 	pass
 
 func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
+	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
 	pass
