38351b97ef365ba408fa7b87daef6e81fd26f29a
minettodev <minetto_dev@protonmail.com>
2022-10-18 17:31:40 -0300

    Inimigos, caminhos, pulo

--- Player.gd
+++ Player.gd
@@ -8,9 +8,13 @@ export(Resource) var moveData = preload("res://DefaultPlayerMovementData.tres") 
 var velocity = Vector2.ZERO
 var state = MOVE
 var double_jump = 1
+var buffered_jump = false
+var coyote_jump = false
 
-onready var animatedSprite = $AnimatedSprite
-onready var ladderCheck = $LadderCheck
+onready var animatedSprite: = $AnimatedSprite
+onready var ladderCheck: = $LadderCheck
+onready var jumpBufferTimer: = $JumpBufferTimer
+onready var coyoteJumpTimer: = $CoyoteJumpTimer
 
 func _physics_process(delta):
 	var input = Vector2.ZERO
@@ -30,37 +34,42 @@ func move_state(input):
 		state = CLIMB
 	
 	apply_gravity()
-	if input.x == 0:
+	
+	if not horizontal_move(input):
 		apply_friction()
 		animatedSprite.animation = "Idle"
 	else:
 		apply_acceleration(input.x)
 		animatedSprite.animation = "Run"
-		
 		animatedSprite.flip_h = input.x > 0
 	
-	if is_on_floor():
-		double_jump = moveData.DOUBLE_JUMP_COUNT
-		if Input.is_action_just_pressed("ui_up"):
-			velocity.y = moveData.JUMP_FORCE
-	else:
+	if is_on_floor(): 
+		reset_double_jump()
+	else: 
 		animatedSprite.animation = "Jump"
-		if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
-			velocity.y = moveData.JUMP_RELEASE_FORCE
 		
-		if Input.is_action_just_pressed("ui_up") and double_jump > 0:
-			velocity.y += moveData.JUMP_FORCE
-			double_jump -= 1
-		
-		if velocity.y > 0:
-			velocity.y += moveData.ADDITIONAL_FALL_GRAVITY
+	if can_jump(): 
+		input_jump()
+	else:
+		input_jump_release()
+		input_double_jump()
+		buffered_jump()
+		fast_fall()
 	
 	var was_in_air = not is_on_floor() 
+	var was_on_floor = is_on_floor()
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
 	var jus_landed = is_on_floor() and was_in_air
 	if jus_landed:
 		animatedSprite.animation = "Run"
 		animatedSprite.frame = 1
+		
+	var just_left_ground = not is_on_floor() and was_on_floor
+	if just_left_ground and velocity.y >= 0:
+		coyote_jump = true
+		coyoteJumpTimer.start()
 
 func climb_state(input):
 	if not is_on_ladder(): state = MOVE
@@ -71,6 +80,41 @@ func climb_state(input):
 	velocity = input * moveData.CLIMB_SPEED
 	velocity = move_and_slide(velocity, Vector2.UP)
 	pass
+
+func input_jump_release():
+	if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
+		velocity.y = moveData.JUMP_RELEASE_FORCE
+
+func input_double_jump():
+	if Input.is_action_just_pressed("ui_up") and double_jump > 0:
+		velocity.y += moveData.JUMP_FORCE
+		double_jump -= 1
+	
+func buffered_jump():
+	if Input.is_action_just_pressed("ui_up"):
+		buffered_jump = true
+		jumpBufferTimer.start()
+		$uui.play()
+
+func fast_fall():
+	if velocity.y > 0:
+		velocity.y += moveData.ADDITIONAL_FALL_GRAVITY
+
+func reset_double_jump():
+		double_jump = moveData.DOUBLE_JUMP_COUNT
+
+func input_jump():
+		if Input.is_action_just_pressed("ui_up") or buffered_jump:
+			velocity.y = moveData.JUMP_FORCE
+			buffered_jump = false
+			$uui.play()
+			
+
+func can_jump():
+	return is_on_floor() or coyote_jump
+
+func horizontal_move(input):
+	return input.x !=0
 
 func is_on_ladder():
 	if not ladderCheck.is_colliding(): return false
@@ -85,3 +129,9 @@ func apply_friction():
 func apply_acceleration(amount):
 	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
 	pass
+
+func _on_JumpBufferTimer_timeout():
+	buffered_jump = false
+
+func _on_CoyoteJumpTimer_timeout():
+	coyote_jump = false
